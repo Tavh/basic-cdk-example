84edb3db3e2888f0a2a10c60a80c35a71b39048e
Tav <therzlic@cisco.com>
2022-08-04 11:42:13 +0300

    ec2 stack

--- basic_cdk_example/basic_cdk_example_stack.py
+++ basic_cdk_example/basic_cdk_example_stack.py
@@ -1,19 +1,43 @@
+import os.path
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_iam as iam
 )
 from constructs import Construct
+from aws_cdk.aws_s3_assets import Asset
+
+dirname = os.path.dirname(__file__)
 
 class BasicCdkExampleStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # VPC
+        vpc = ec2.Vpc(
+            self, 
+            "VPC",
+            nat_gateways=0,
+            subnet_configuration=[ec2.SubnetConfiguration(name="public",subnet_type=ec2.SubnetType.PUBLIC)]
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "BasicCdkExampleQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # AMI
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+        )
+
+        # Instance Role and SSM Managed Policy
+        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
+
+        # Instance
+        instance = ec2.Instance(self, "Instance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=amzn_linux,
+            vpc = vpc,
+            role = role
+        )
